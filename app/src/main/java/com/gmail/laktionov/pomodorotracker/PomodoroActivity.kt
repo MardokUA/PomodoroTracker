26726f703ed123ea28737dd06dff18c5ac47812a
MardokUA <gbrfxe4177>
2018-07-26 12:15:04 +0300

    - initial commit

---
+++ app/src/main/java/com/gmail/laktionov/pomodorotracker/PomodoroActivity.kt
@@ -0,0 +1,12 @@
+package com.gmail.laktionov.pomodorotracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PomodoroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pomodoro)
+    }
+}
